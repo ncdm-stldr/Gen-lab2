23bed0f67d7412945a1db6aa3d7754e04545444a
ncdm-stldr <ncdm.stldr@gmail.com>
2019-03-27 22:24:37 +0100

    page 34.3, issue 12

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -9,8 +9,8 @@ public class Franc extends Money{
     }
 
     public boolean equals(Object object) {
-        Money franc = (Money) object;
-        return amount == franc.amount;
+        Money money = (Money) object;
+        return amount == money.amount;
     }
 
 }
