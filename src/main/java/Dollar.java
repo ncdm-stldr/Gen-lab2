01144515cd4d683f7d182935ff82296fcda51a35
ncdm-stldr <ncdm.stldr@gmail.com>
2019-03-27 22:11:27 +0100

    page 33.2, issue 12

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -9,7 +9,7 @@ public class Dollar extends Money {
     }
 
     public boolean equals(Object object) {
-        Money dollar = (Dollar) object;
+        Money dollar = (Money) object;
         return amount == dollar.amount;
     }
 
